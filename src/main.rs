57e2e30218a8653d03357f5626d1e9d489c6bed3
hchap1 <hchap1@hotmail.com>
2025-09-26 14:29:29 +1000

    chore: add issue to main.rs to fix later

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,15 @@ mod backend;
 mod error;
 
 fn main() -> iced::Result {
+
+    /*
+
+        Current issue:
+
+        server/client ID are not the same, so they must be stored in pairs.
+        When a connection is requested to a server, the client requesting the connection must send the ID of its server so that it may be messaged back.
+
+    */
     iced::application("Pingpong", Application::update, Application::view)
         .run_with(|| (Application::default(), Message::Global(Global::StartNetworkRelays).task()))
 }
